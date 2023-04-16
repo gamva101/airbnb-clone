282d7f2a35fe64a91c3c0ac8794b2240050ba913
hyunehol <hyunchul14@gmail.com>
2023-04-16 19:01:42 +0900

    reservation component added

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,17 +1,24 @@
-import { Listing, User } from "@prisma/client";
+import { Listing, Reservation, User } from '@prisma/client';
 
 export type SafeUser = Omit<
-    User,
-    "createdAt" | "updatedAt" | "emailVerified"
-    > & {
-    createdAt: string;
-    updatedAt: string;
-    emailVerified: string | null;
-}
+  User,
+  'createdAt' | 'updatedAt' | 'emailVerified'
+> & {
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+};
 
-export type SafeListing = Omit<
-    Listing,
-    "createAt"
-    > & {
-    createAt: string;
+export type SafeReservation = Omit<
+  Reservation,
+  'createdAt' | 'startDate' | 'endDate' | 'listing'
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
+
+export type SafeListing = Omit<Listing, 'createAt'> & {
+  createAt: string;
 };
